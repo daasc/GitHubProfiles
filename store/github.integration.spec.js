d1efffee4120eabb899dc6b19c15df062d7304fa
daasc <paulojhol@gmail.com>
2022-03-23 21:15:32 -0300

    add test integration

---
+++ store/github.integration.spec.js
@@ -0,0 +1,44 @@
+import { createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import axios from 'axios'
+import { users, repository } from '../db/test.json'
+import { state, mutations, getters, actions } from '@/store/github.js'
+jest.mock('axios')
+
+const storeConfig = {
+  state,
+  mutations,
+  getters,
+  actions,
+  namespaced: true,
+}
+describe('GitHub', () => {
+  const createStore = (getUser = true) => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+    if (getUser) {
+      axios.get = jest
+        .fn()
+        .mockImplementationOnce(() => Promise.resolve({ data: users }))
+    } else {
+      axios.get = jest
+        .fn()
+        .mockImplementationOnce(() => Promise.resolve({ data: repository }))
+    }
+    const store = new Vuex.Store(storeConfig)
+    return { store }
+  }
+
+  it('should make a get call getting the user data', async () => {
+    const { store } = createStore()
+    await store.dispatch('getUsers', { search: 'daasc' })
+    expect(store.state.users).toEqual(users)
+  })
+
+  it("should perform a get call getting the searched user's repository", async () => {
+    const { store } = createStore(false)
+    store.commit('SET_USERS', users)
+    await store.dispatch('getRepository')
+    expect(store.state.repository).toEqual(repository)
+  })
+})
--- store/github.unit.spec.js
+++ store/github.unit.spec.js
@@ -1,9 +1,7 @@
 import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
-import axios from 'axios'
-import { users } from '../db/test.json'
+import { users, repository } from '../db/test.json'
 import { state, mutations, getters, actions } from '@/store/github.js'
-jest.mock('axios')
 
 const storeConfig = {
   state,
@@ -16,9 +14,6 @@ describe('GitHub', () => {
   const createStore = () => {
     const localVue = createLocalVue()
     localVue.use(Vuex)
-    axios.get = jest
-      .fn()
-      .mockImplementationOnce(() => Promise.resolve({ data: { results: '' } }))
     const store = new Vuex.Store(storeConfig)
     return { store }
   }
@@ -26,9 +21,18 @@ describe('GitHub', () => {
     const { store } = createStore()
     expect(store.state.users).toEqual({})
   })
+  it('should return the value of the repository ', () => {
+    const { store } = createStore()
+    expect(store.state.repository).toEqual([])
+  })
   it('should add value in users when SET_USERS is called', () => {
     const { store } = createStore()
     store.commit('SET_USERS', users)
     expect(store.state.users).toEqual(users)
+  })
+  it('should add value in users when SET_USERS is called', () => {
+    const { store } = createStore()
+    store.commit('SET_REPOSITORY', repository)
+    expect(store.state.repository).toEqual(repository)
   })
 })

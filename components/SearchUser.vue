4048be63992291168bc90609e6b67f7b90c1a2b4
daasc <paulojhol@gmail.com>
2022-03-25 20:48:38 -0300

    add search and your test

---
+++ components/SearchUser.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="search">
+    <input
+      v-model="search"
+      date-testid="input-search"
+      type="search"
+      name=""
+      class="search__input"
+    />
+    <button class="search__button" date-testid="search" @click="searchUser()">
+      search
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SearchUser',
+  data() {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    searchUser() {
+      this.$emit('doSearch', { term: this.search })
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.search {
+  display: flex;
+  margin: 50px;
+  .search__input {
+    padding: 10px;
+    border: 1px solid rgb(170, 170, 170);
+    outline: none;
+    border-right: none;
+  }
+  .search__button {
+    border: 1px solid;
+    border-left: none;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <nav-bar></nav-bar>
+    <search-user></search-user>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import SearchUser from '@/components/SearchUser.vue'
 export default {
   name: 'IndexPage',
-  components: { NavBar },
+  components: { NavBar, SearchUser },
 }
 </script>
 <style lang=""></style>

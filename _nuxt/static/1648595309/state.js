f090f31254a3b7e737e5118c1067738ca6c25f19
daasc <paulojhol@gmail.com>
2022-03-29 20:08:44 -0300

    Updates

---
+++ _nuxt/static/1648595309/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FGitHubProfiles\u002F_nuxt\u002Fstatic\u002F1648595309",layout:"default",error:a,state:{github:{users:{},repository:[]}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FGitHubProfiles\u002F",assetsPath:"\u002FGitHubProfiles\u002F_nuxt\u002F",cdnURL:a}}}}(null));
